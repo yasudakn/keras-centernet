9c0a8394142bb90459b99b3fe9d5ef788eaf024f
yasudakn <yasudakn@gmail.com>
2019-08-01 10:54:59 +0900

    add csv output

--- keras_centernet/bin/ctdet_image.py
+++ keras_centernet/bin/ctdet_image.py
@@ -5,6 +5,8 @@ import numpy as np
 import os
 from tqdm import tqdm
 from glob import glob
+import pandas as pd
+import math
 
 from keras_centernet.models.networks.hourglass import HourglassNetwork, normalize_image
 from keras_centernet.models.decode import CtDetDecode
@@ -17,8 +19,9 @@ def main():
   parser.add_argument('--fn', default='assets/demo.jpg', type=str)
   parser.add_argument('--output', default='output', type=str)
   parser.add_argument('--inres', default='512,512', type=str)
-  parser.add_argument('--batch-size', default=32)
-  parser.add_argument('--classes', default='', nargs='*')
+  parser.add_argument('--batch-size', default=32, type=int)
+  parser.add_argument('--target-classes', default=[0], nargs='*', type=int)
+  parser.add_argument('--under-score', default=0.3, type=float)
   args, _ = parser.parse_known_args()
   args.inres = tuple(int(x) for x in args.inres.split(','))
   os.makedirs(args.output, exist_ok=True)
@@ -38,17 +41,21 @@ def main():
   drawer = COCODrawer()
   fns = sorted(glob(args.fn))
   fns_iter = iter(fns)
+  pred_df = []
   
-  print('{}/{} {}'.format(len(fns), args.batch_size, int(len(fns) / args.batch_size)))
+  batch_loop_count = math.ceil(len(fns) / args.batch_size)
+  print('{}/batch_size({})={} batch loop'.format(len(fns), args.batch_size, batch_loop_count))
   
-  for j in range(0, int(len(fns) / args.batch_size)):
+  for j in range(0, batch_loop_count):
     pimgs = np.empty((0, args.inres[0], args.inres[1], 3))
     fnames = []
-    for i in range(0, args.batch_size):
+    last_batch = len(fns) % args.batch_size if (j == batch_loop_count-1) else args.batch_size
+    for i in range(0, last_batch):
       fname = next(fns_iter)
       img = cv2.imread(fname)
       letterbox_transformer = LetterboxTransformer(args.inres[0], args.inres[1])
       pimg = letterbox_transformer(img)
+#      cv2.imwrite(fname.replace('.jpg', '_lb.jpg'), pimg)
       pimg = normalize_image(pimg)
       pimg = np.expand_dims(pimg, 0)
       pimgs = np.append(pimgs, pimg, axis=0)
@@ -62,16 +69,22 @@ def main():
       img = cv2.imread(fn)
       for d in p:
         x1, y1, x2, y2, score, cl = d
-        if 0 < len(args.classes) and cl in args.classes:
-          if score < 0.3:
-            break
-          x1, y1, x2, y2 = letterbox_transformer.correct_box(x1, y1, x2, y2)
-          img = drawer.draw_box(img, x1, y1, x2, y2, cl)
+        x1, y1, x2, y2 = letterbox_transformer.correct_box(x1, y1, x2, y2)
+        if score < args.under_score:
+          break
+        if 0 == len(args.target_classes) or cl in args.target_classes:
+          img = drawer.draw_box(img, x1, y1, x2, y2, score, cl)
+          pred_df.append([os.path.basename(fn), x1, y1, x2, y2, score, cl])
 
       out_fn = os.path.join(args.output, 'ctdet.' + os.path.basename(fn))
       cv2.imwrite(out_fn, img)
       print("Image saved to: %s" % out_fn)
 
+  df = pd.DataFrame(pred_df, columns=['fname', 'x1', 'y1', 'x2', 'y2', 'score', 'cl'])
+  df['cl'] = df['cl'].astype(int)
+  out_df = os.path.join(args.output, 'ctdet.csv')
+  df.to_csv(out_df, index=False)
+  print('output csv {} size: {}'.format(out_df, len(pred_df)))
 
 if __name__ == '__main__':
   main()
